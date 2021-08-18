c6cc3d2dc3e9f619941c6213566cec6b4024ebc3
Joon-Hyuk Ko <jhko725@seas.harvard.edu>
2021-08-18 14:34:51 -0400

    Code for plotting the 3D volume written

--- czi_converter.py
+++ czi_converter.py
@@ -1,14 +1,21 @@
 #%%
 import numpy as np
 from aicsimageio import AICSImage
+from aicsimageio.writers import OmeTiffWriter
 
-img = AICSImage('./Data/Fed_X63_Z3_SIM.czi')
+czi_file = AICSImage('./Data/Fed_X63_Z3_SIM.czi')
+dim_order = 'CZYX'
+img_data = czi_file.get_image_data(dim_order, T = 0)
+channel_names = ['desmin', 'actin']
+
 # %%
-img.data
+savepath = './Data/Fed_X63_Z3_SIM.ome.tiff'
+OmeTiffWriter.save(img_data, savepath, dim_order = dim_order, channel_names = channel_names, physical_pixel_sizes = czi_file.physical_pixel_sizes)
 # %%
-scale = tuple(img.physical_pixel_sizes)
-# %%
-data_dict = {'data': img.data, 'scale': scale, 'channels': ('actin', 'desmin')}
-# %%
-np.savez('./Data/Fed_X63_Z3_SIM.npz', **data_dict)
-# %%
+import h5py
+with h5py.File('./Data/Fed_X63_Z3_SIM.h5', 'w') as f:
+    dataset = f.create_dataset("Data", data = img_data)
+    dataset.attrs['dim_order'] = dim_order
+    dataset.attrs['channels'] = channel_names
+    dataset.attrs['pixel_sizes'] = np.array(czi_file.physical_pixel_sizes, dtype = np.float32)
+
--- test.jl
+++ test.jl
@@ -4,6 +4,115 @@ using GLMakie
 using Pkg
 Pkg.activate(".")
 using ProteinStructureReconstruction
+using HDF5
+using Images
+import Statistics: mean, median
+import GeometryBasics: Point2f0
 ##
-a = plot(rand(10))
-display(a)
+filepath = "./Data/Fed_X63_Z3_SIM.h5"
+h5file = h5open(filepath, "r")
+##
+# TODO: don't load everything in memory at once -  just draw overlay first, then 
+##
+x_range, y_range = 2000:2300, 1600:1900
+img_data = h5file["Data"][x_range, y_range, :, :] |> x -> reinterpret(N0f16, x)
+##
+function make_overlay(zstack_dataset::AbstractArray; dims = 0)
+    overlay = zstack_dataset |> x -> reinterpret(N0f16, x) |> x ->  mean(x, dims = dims) |> x -> dropdims(x, dims = dims)
+    return overlay
+end
+##
+overlay = make_overlay(read(h5file["Data"]), dims = 3)
+##
+
+let
+    x1, x2 = first(x_range), last(x_range)
+    y1, y2 = first(y_range), last(y_range)
+    rect = Point2f0[(x1, y1), (x1, y2), (x2, y2), (x2, y1)]
+    fig = Figure(resolution = (1500, 800))
+    ax1 = GLMakie.Axis(fig[1, 1], title = "Desmin overlay", titlesize = 20.0f0)
+    ax2 = GLMakie.Axis(fig[1, 2], title = "Actin overlay", titlesize = 20.0f0)
+    axes = [ax1, ax2]
+    for (i, ax) in enumerate(axes)
+        image!(ax, overlay[:, :, i])
+        poly!(ax, rect, color = nothing, strokecolor = :red, strokewidth = 2.0)
+    end
+    fig
+end
+##
+let
+    fig = Figure(resolution = (1500, 800))
+    ax1 = GLMakie.Axis(fig[1, 1], title = "Desmin overlay", titlesize = 20.0f0)
+    ax2 = GLMakie.Axis(fig[1, 2], title = "Actin overlay", titlesize = 20.0f0)
+    axes = [ax1, ax2]
+    for (i, ax) in enumerate(axes)
+        image!(ax, overlay[x_range, y_range, i])
+    end
+    fig
+end
+##
+size(h5file["Data"])
+##
+function plot_image_histogram(image:AbstractArray)
+    flattened = vec(image)
+
+end
+##
+image(overlay[1500:2000, 2000:2500, 2])
+##
+import StatsBase: fit, Histogram
+let 
+    fig = Figure()
+    axes = [GLMakie.Axis(fig[1, 1]), GLMakie.Axis(fig[1, 2])]
+    Δz = 7
+    for i in 1:Δz:size(h5file["Data"])[3], j in 1:size(h5file["Data"])[4]
+        print(i)
+        img = h5file["Data"][:, :, i, j]
+        flat_img = vec(img)
+        density!(axes[j], flat_img[flat_img .> 0], label = "z slice #$(i)")
+    end
+    
+    fig
+end
+##
+let 
+    fig = Figure()
+    means = Array{Float32}(undef, size(h5file["Data"])[3:4]...)
+    for i in 1:size(h5file["Data"])[3], j in 1:size(h5file["Data"])[4]
+        img = h5file["Data"][:, :, i, j]
+        flat_img = vec(img)
+        means[i, j] = median(flat_img[flat_img .> 0])
+    end
+    
+    axes = [GLMakie.Axis(fig[1, 1]), GLMakie.Axis(fig[1, 2])]
+    for (i, ax) in enumerate(axes)
+        plot!(ax, means[:, i])
+    end
+    fig
+end
+##
+
+##
+img_hist = fit(Histogram, vec(overlay[:, :, 2]), nbins = 30)
+barplot(img_hist)
+##
+let 
+    fig = Figure(resolution = (1200, 800))
+    axes = [Axis3(fig[1, 1], title = "Actin (Raw)"), Axis3(fig[1, 2], title = "Desmin (Raw)")]
+    cmaps = [:Greens_3, :Reds_3]
+    for (i, ax) in enumerate(axes)
+        contour!(ax, img_data[:, :, :, i], levels = 4, alpha = 0.4, colormap = cmaps[i])
+    end
+    
+    fig
+end
+##
+function equalize_zstack(zstack::AbstractArray)
+    equalizer = Equalization(nbins = 256, minval = 0.0, maxval = 0.8)
+    out = mapslices(x -> adjust_histogram(x, equalizer), zstack, dims = [1, 2])
+    return out
+end
+##
+read(h5file["Data"]["channels"])
+##
+img_eq = equalize_zstack(img_data)
